ec3c31b40d5780ad2d81dee5e242e9340e76baa9
Rohan Tulsiani <rohantulsiani@gmail.com>
2021-04-24 14:36:12 +0800

    initial commit

---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+
+int main(int argc, char **argv)
+{
+	glfwInit();
+	glm::vec3 myVector(1.0);
+	std::cout << myVector.x << std::endl;
+	return 0;
+}
